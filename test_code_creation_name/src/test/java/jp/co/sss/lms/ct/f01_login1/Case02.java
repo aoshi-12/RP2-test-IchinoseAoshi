bf047aba3a49bd2ce74f8489def55c4ab2fa0eff
aoshi <a-ichinose@piolet.co.jp>
2025-12-23 18:17:05 +0900

    fix:Case02　修正
    レビューの指摘内容の修正

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import jp.co.sss.lms.ct.util.WebDriverUtils;
 
@@ -20,7 +25,6 @@ import jp.co.sss.lms.ct.util.WebDriverUtils;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース02 受講生 ログイン 認証失敗")
 public class Case02 {
-	WebDriverUtils utils = new WebDriverUtils();
 
 	/** 前処理 */
 	@BeforeAll
@@ -40,18 +44,44 @@ public class Case02 {
 	void test01() {
 		//テスト処理
 		goTo("http://localhost:8080/lms/");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
 		//エビデンスの取得
-		getEvidence(this);
+		getEvidence(new Object() {
+		});
+	}
+
+	//ログインID	
+	@FindBy(name = "loginId")
+	private WebElement loginIdInput;
+	//パスワード
+	@FindBy(name = "password")
+	private WebElement passwordInput;
+	//ログインボタン
+	@FindBy(css = ".btn.btn-primary")
+	private WebElement loginButton;
+
+	public void login(String id, String pw) {
+		PageFactory.initElements(webDriver, this);
+
+		clearAndSendKeys(loginIdInput, id);
+		clearAndSendKeys(passwordInput, pw);
+		clickLoginButton(loginButton);
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
+
+		PageFactory.initElements(WebDriverUtils.webDriver, this);
+		login("StudentAA0111", "StudentAA0111");
+
 		//テスト処理
-		utils.login("StudentAA0111", "StudentAA0111");
+		String errorMessage = webDriver.findElement(By.cssSelector(".help-inline.error")).getText();
+		assertEquals("* ログインに失敗しました。", errorMessage);
 		//エビデンスの取得
-		getEvidence(this);
+		getEvidence(new Object() {
+		});
 	}
 
 }
