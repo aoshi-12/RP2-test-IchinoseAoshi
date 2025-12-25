cf4f28b8179ddab4f038b484fec1c55ecdbad7c2
aoshi <a-ichinose@piolet.co.jp>
2025-12-25 16:22:43 +0900

    add:Case05　テストコード追加
    キーワード検索のテストコードの作成

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
@@ -3,6 +3,8 @@ package jp.co.sss.lms.ct.f02_faq;
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -11,7 +13,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト よくある質問機能
@@ -38,41 +40,102 @@ public class Case05 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		//テスト処理
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		WebElement loginId = webDriver.findElement(By.id("loginId"));
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+		WebElement password = webDriver.findElement(By.id("password"));
+		password.clear();
+		password.sendKeys("Aa123456");
+		WebElement loginButton = webDriver.findElement(By.cssSelector("input[type='submit'][value='ログイン']"));
+		loginButton.click();
+		//テスト処理
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
-	
+
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		WebElement menuToggle = webDriver.findElement(By.className("dropdown-toggle"));
+		menuToggle.click();
+		WebElement help = webDriver.findElement(By.linkText("ヘルプ"));
+		help.click();
+		//テスト処理
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		String currentWindow = webDriver.getWindowHandle();
+		WebElement faq = webDriver.findElement(By.linkText("よくある質問"));
+		faq.click();
+
+		for (String windowHandle : webDriver.getWindowHandles()) {
+			if (!windowHandle.equals(currentWindow)) {
+				webDriver.switchTo().window(windowHandle);
+				break;
+			}
+		}
+		//テスト処理
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
+
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 キーワード検索で該当キーワードを含む検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+		WebElement form = webDriver.findElement(By.id("form"));
+		form.clear();
+		form.sendKeys("研修");
+		WebElement button = webDriver.findElement(By.cssSelector("input[type='submit']"));
+		button.click();
+		//テスト処理
+		List<WebElement> results = webDriver.findElements(By.cssSelector("dt.mb10 > span:last-child"));
+		for (WebElement result : results) {
+			assertTrue(result.getText().contains("研修"));
+		}
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
-	
+
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 「クリア」ボタン押下で入力したキーワードを消去")
 	void test06() {
-		// TODO ここに追加
+		WebElement clear = webDriver.findElement(By.cssSelector("input[value='クリア']"));
+		clear.click();
+
+		//テスト処理
+		WebElement form = webDriver.findElement(By.id("form"));
+		assertEquals("", form.getText());
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
